3916123c14515c5843d2b0c9ed84ba0fe5e06998
kundan-kumavat <kundankumawat003@gmail.com>
2025-10-10 16:28:04 +0530

    Prisma Setup Done

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
+  await app.listen(process.env.PORT ? Number(process.env.PORT) : 4000);
+  console.log(`Backend Listening on ${await app.getUrl()}`);
 }
 bootstrap();
